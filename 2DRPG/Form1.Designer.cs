81588faef46135e3ef169411d46ba7057028f659
thejereman13 <jereman13@gmail.com>
2017-06-13 13:22:54 -0400

    Visual Studio Project Import
    Setup a visual studio windows form project and added some sample code
    from the OpenGL.Net repo.

---
+++ 2DRPG/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿using OpenGL;
+namespace _2DRPG {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.RenderControl = new OpenGL.GlControl();
+            this.SuspendLayout();
+
+            this.RenderControl.Animation = true;
+			this.RenderControl.BackColor = System.Drawing.Color.DimGray;
+			this.RenderControl.ColorBits = ((uint)(24u));
+			this.RenderControl.DepthBits = ((uint)(0u));
+			this.RenderControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.RenderControl.Location = new System.Drawing.Point(0, 0);
+			this.RenderControl.MultisampleBits = ((uint)(0u));
+			this.RenderControl.Name = "RenderControl";
+			this.RenderControl.Size = new System.Drawing.Size(731, 428);
+			this.RenderControl.StencilBits = ((uint)(0u));
+			this.RenderControl.TabIndex = 0;
+			this.RenderControl.ContextCreated += new System.EventHandler<OpenGL.GlControlEventArgs>(this.RenderControl_ContextCreated);
+			this.RenderControl.ContextDestroying += new System.EventHandler<OpenGL.GlControlEventArgs>(this.RenderControl_ContextDestroying);
+			this.RenderControl.Render += new System.EventHandler<OpenGL.GlControlEventArgs>(this.RenderControl_Render);
+			this.RenderControl.ContextUpdate += new System.EventHandler<OpenGL.GlControlEventArgs>(this.RenderControl_ContextUpdate);
+			// 
+			// SampleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(731, 428);
+			this.Controls.Add(this.RenderControl);
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "Test Form 1";
+        }
+
+        #endregion;
+        private OpenGL.GlControl RenderControl;
+    }
+}
+
