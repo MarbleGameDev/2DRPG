e677ac8ee935b632610f5c7d124b01c53b0655c4
thejereman13 <jereman13@gmail.com>
2017-06-13 15:23:45 -0400

    Whoops
    Must have dropped this file along the way.

--- 2DRPG/Form1.cs
+++ 2DRPG/Form1.cs
@@ -7,17 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using OpenGL;
 
 namespace _2DRPG {
     public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
+		public Form1() {
+			InitializeComponent();
+		}
 
-        private void RenderControl_ContextCreated(object sender, GlControlEventArgs e)
-		{
+		private void RenderControl_ContextCreated(object sender, GlControlEventArgs e) {
 			GlControl glControl = (GlControl)sender;
 			// Here you can allocate resources or initialize state
 			Gl.MatrixMode(MatrixMode.Projection);
@@ -29,51 +27,35 @@ namespace _2DRPG {
 				Gl.Enable(EnableCap.Multisample);
 		}
 
-		private void RenderControl_Render(object sender, GlControlEventArgs e)
-		{
-				RenderControl_Render_GL(sender, e);
+		private void RenderControl_Render(object sender, GlControlEventArgs e) {
+			RenderControl_Render_GL(sender, e);
 		}
 
-		private void RenderControl_ContextUpdate(object sender, GlControlEventArgs e)
-		{
+		private void RenderControl_ContextUpdate(object sender, GlControlEventArgs e) {
+			foreach (IRenderObject o in Program.objects) {
+				o.ContextUpdate();
+			}
 			// Change triangle rotation
-			_Angle = (_Angle + 0.1f) % 45.0f;
+
 		}
 
-		private void RenderControl_ContextDestroying(object sender, GlControlEventArgs e)
-		{
+		private void RenderControl_ContextDestroying(object sender, GlControlEventArgs e) {
 			// Here you can dispose resources allocated in RenderControl_ContextCreated
+			foreach (IRenderObject o in Program.objects) {
+				o.ContextDestroyed();
+			}
 		}
 
 		#region Common Data
 
-		private static float _Angle;
 
-		/// <summary>
-		/// Vertex position array.
-		/// </summary>
-		private static readonly float[] _ArrayPosition = new float[] {
-			0.0f, 0.0f,
-			0.5f, 1.0f,
-			1.0f, 0.0f
-		};
-
-		/// <summary>
-		/// Vertex color array.
-		/// </summary>
-		private static readonly float[] _ArrayColor = new float[] {
-			1.0f, 0.0f, 0.0f,
-			0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 1.0f
-		};
 
 		#endregion
 
 		#region GL Resources
 
-		private void RenderControl_Render_GL(object sender, GlControlEventArgs e)
-		{
-            Control senderControl = (Control)sender;
+		private void RenderControl_Render_GL(object sender, GlControlEventArgs e) {
+			Control senderControl = (Control)sender;
 
 			Gl.Viewport(0, 0, senderControl.ClientSize.Width, senderControl.ClientSize.Height);
 			Gl.Clear(ClearBufferMask.ColorBufferBit);
@@ -81,37 +63,12 @@ namespace _2DRPG {
 			// Animate triangle
 			Gl.MatrixMode(MatrixMode.Modelview);
 			Gl.LoadIdentity();
-			Gl.Rotate(_Angle, 0.0f, 0.0f, 1.0f);
 
-			if (Gl.CurrentVersion >= Gl.Version_110) {
-				// Old school OpenGL 1.1
-				// Setup & enable client states to specify vertex arrays, and use Gl.DrawArrays instead of Gl.Begin/End paradigm
-				using (MemoryLock vertexArrayLock = new MemoryLock(_ArrayPosition)) 
-				using (MemoryLock vertexColorLock = new MemoryLock(_ArrayColor))
-				{
-					// Note: the use of MemoryLock objects is necessary to pin vertex arrays since they can be reallocated by GC
-					// at any time between the Gl.VertexPointer execution and the Gl.DrawArrays execution
-
-					Gl.VertexPointer(2, VertexPointerType.Float, 0, vertexArrayLock.Address);
-					Gl.EnableClientState(EnableCap.VertexArray);
-
-					Gl.ColorPointer(3, ColorPointerType.Float, 0, vertexColorLock.Address);
-					Gl.EnableClientState(EnableCap.ColorArray);
-
-					Gl.DrawArrays(PrimitiveType.Triangles, 0, 3);
-				}
-			} else {
-				// Old school OpenGL
-				Gl.Begin(PrimitiveType.Triangles);
-				Gl.Color3(1.0f, 0.0f, 0.0f); Gl.Vertex2(0.0f, 0.0f);
-				Gl.Color3(0.0f, 1.0f, 0.0f); Gl.Vertex2(0.5f, 1.0f);
-				Gl.Color3(0.0f, 0.0f, 1.0f); Gl.Vertex2(1.0f, 0.0f);
-				Gl.End();
+			// Setup & enable client states to specify vertex arrays, and use Gl.DrawArrays instead of Gl.Begin/End paradigm
+			foreach (IRenderObject o in Program.objects) {
+				o.Render();
 			}
 		}
-
 		#endregion
-
-
-    }
+	}
 }
